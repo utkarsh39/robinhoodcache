610a307ff01ab513e9e7b9493dd75ae0214e8cc4
Utkarsh <utkarsh39@hotmail.com>
2019-02-22 12:55:58 -0500

    Added config parameters to redis

--- nuapp/start.sh
+++ nuapp/start.sh
@@ -11,7 +11,14 @@ pushd src
 curl -L ${CONFIG_URL}/${CONFIG}.tar.gz -o ${CONFIG}.tar.gz
 tar -xzvf ${CONFIG}.tar.gz
 mv config /config
-redis-server&
+rm -f redis.conf
+touch redis.conf
+echo "maxmemory 1gb" >> redis.conf
+echo "maxmemory-policy allkeys-lru" >> redis.conf
+echo "appendonly no" >> redis.conf
+echo "save \"\"" >> redis.conf
+
+redis-server redis.conf&
 # ./start_controller.sh ${eth0} &
 ./start_cache.sh ${eth0} &
 ./start_app_server.sh ${eth0} &
