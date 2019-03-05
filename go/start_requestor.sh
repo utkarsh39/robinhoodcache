9e2f3c7b8b0c28e67bce505e979d5ac4fda20e86
Ubuntu <ubuntu@ip-172-31-50-180.ec2.internal>
2019-03-05 16:38:03 +0000

    Fixed bug in cooying trace file to container

--- go/start_requestor.sh
+++ go/start_requestor.sh
@@ -9,13 +9,14 @@ fi
 curl -L ${CONFIG_URL}/${CONFIG}.tar.gz -o ${CONFIG}.tar.gz
 tar -xzvf ${CONFIG}.tar.gz
 mv config /config
-pushd /bootstrap
+#pushd /bootstrap
 #if [[ ! -e "./trace.json" || $DOWNLOAD ]]
 #then
 #    curl -L $TRACE_URL > trace.json.tar.gz || (echo "curl failed: $TRACE_URL" && rm trace.json.tar.gz&& exit 5)
 #    tar -zxf /bootstrap/trace.json.tar.gz || (echo "extract failed" && rm -rf trace.json && exit 5)
 #fi
 #popd
+mkdir -p /bootstrap
 mv /boot/trace.json /bootstrap/trace.json
 cd requestor
 go-wrapper run &> /logs/requestor_$(date +%Y%m%d-%H%M%S)
