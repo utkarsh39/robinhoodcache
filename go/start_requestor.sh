7fc3077c667c3570a1e7c4a9471d3ad76ead0ad2
Utkarsh <utkarsh39@hotmail.com>
2019-03-13 01:17:35 -0400

    Uploading config from local directory now

--- go/start_requestor.sh
+++ go/start_requestor.sh
@@ -6,17 +6,18 @@ elif [[ ! $SKIP_UNPACK ]]
 then
     sleep 10m
 fi
-curl -L ${CONFIG_URL}/${CONFIG}.tar.gz -o ${CONFIG}.tar.gz
-tar -xzvf ${CONFIG}.tar.gz
-mv config /config
-pushd /bootstrap
-if [[ ! -e "./trace.json" || $DOWNLOAD ]]
-then
-    curl -L $TRACE_URL > trace.json.tar.gz || (echo "curl failed: $TRACE_URL" && rm trace.json.tar.gz&& exit 5)
-    tar -zxf /bootstrap/trace.json.tar.gz || (echo "extract failed" && rm -rf trace.json && exit 5)
-fi
-popd
-
+# curl -L ${CONFIG_URL}/${CONFIG}.tar.gz -o ${CONFIG}.tar.gz
+# tar -xzvf ${CONFIG}.tar.gz
+# mv config /config
+#pushd /bootstrap
+#if [[ ! -e "./trace.json" || $DOWNLOAD ]]
+#then
+#    curl -L $TRACE_URL > trace.json.tar.gz || (echo "curl failed: $TRACE_URL" && rm trace.json.tar.gz&& exit 5)
+#    tar -zxf /bootstrap/trace.json.tar.gz || (echo "extract failed" && rm -rf trace.json && exit 5)
+#fi
+#popd
+mkdir -p /bootstrap
+mv /boot/trace.json /bootstrap/trace.json
 cd requestor
 go-wrapper run &> /logs/requestor_$(date +%Y%m%d-%H%M%S)
 
